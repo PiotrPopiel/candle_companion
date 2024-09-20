2979ca7474613cc690bcfa204f374c9e5b96047e
Piotr Popiel <popielpiotr90@gmail.com>
2024-09-20 10:05:46 +0200

    fix mobile dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -6,7 +6,7 @@ export default async function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="mt-24 p-2 flex gap-5 lg:flex-row">
+    <div className="mt-24 p-2 flex flex-col gap-5 lg:flex-row">
       <Sidenav />
       <main className="bg-gray-50 h-[800px] w-full rounded-md p-4 shadow-md">
         {children}
